ff06b68d07c7e474f5906121561a93fc5af80769
Oliver <deftool@outlook.com>
2016-04-18 20:26:49 -0700

    added model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ActiveRecord::Base
+
+DISCOUNT_THRESHOLD = 10
+SALES_TAX = 0.09
+  def sale_message
+    if price < DISCOUNT_THRESHOLD 
+      return "Discount Item"
+    else
+      return "On Sale"
+    end
+  end
+
+  def tax
+    price * SALES_TAX
+  end
+
+  def total
+    price + tax
+    #"#{tax} + #{price}"
+  end
 end
